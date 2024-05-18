77c681f7ba1c36a29d9579a6026fe12bdd517f3e
bhakti <bhaktishinde2388@gmail.com>
2024-05-18 18:46:47 +0530

    modified card

---
+++ js/card3.js
@@ -0,0 +1,36 @@
+const quantityText = document.getElementById("quantity-text");
+const showQuantity = document.getElementById("show-quantity");
+const showTotal = document.getElementById("show-total");
+
+function inc()
+{
+
+let quantity = parseInt(quantityText.innerText);
+
+if(quantity < 5)
+{
+  quantityText.innerText = ++quantity;
+}
+else
+{
+  alert("you cant add more than 5 items")
+}
+showQuantity.innerText = `${quantity} x 1299`;
+showTotal.innerText = `Total:${quantity * 1299}`
+}
+
+function dec()
+{
+let quantity = parseInt(quantityText.innerText);
+
+if(quantity > 1)
+{
+  quantityText.innerText = --quantity;
+}
+else
+{
+  alert("you cant add less than 1 items")
+}
+showQuantity.innerText = `${quantity} x 1299`
+showTotal.innerText = `Total:${quantity * 1299}`
+}
--- js/index.js
+++ js/index.js
@@ -27,23 +27,100 @@ function lightmode(){
 }
 
 //img toggler
-function cake(b)
+function mainImg(b)
 {
-   const flavour = document.getElementById('bulb');
-   if(b=='butter scotch')
+   const number = document.getElementById('bulb');
+   if(b=='number-1')
    {
-   flavour.src = './../img/index-images/store-img3.jpg';
+    number.src = './../img/index-images/store-img3.jpg';
    }
-   else if(b=='chocolate')
+   else if(b=='number-2')
     {
-        flavour.src = './../img/index-images/store-imgg2.jpg';
+        number.src = './../img/index-images/store-imgg2.jpg';
     }
-    else if(b=='Fruit')
+    else if(b=='number-3')
     {
-        flavour.src = './../img/index-images/store-img3 (2).jpg';
+        number.src = './../img/index-images/store-img3 (2).jpg';
     }
    
-    const choice=document.getElementById("flavour");
+    const choice=document.getElementById("number");
 
 choice.innerText=`Flavour :\n ${b}`; 
+}
+
+// products
+const quantityText = document.getElementById("quantity-text");
+const showQuantity = document.getElementById("show-quantity");
+const showTotal = document.getElementById("show-total");
+
+function inc()
+{
+
+let quantity = parseInt(quantityText.innerText);
+
+if(quantity < 5)
+{
+  quantityText.innerText = ++quantity;
+}
+else
+{
+  alert("you cant add more than 5 items")
+}
+showQuantity.innerText = `${quantity} x 799`;
+showTotal.innerText = `Total:${quantity * 799}`
+}
+
+function dec()
+{
+let quantity = parseInt(quantityText.innerText);
+
+if(quantity > 1)
+{
+  quantityText.innerText = --quantity;
+}
+else
+{
+  alert("you cant add less than 1 items")
+}
+showQuantity.innerText = `${quantity} x 799`
+showTotal.innerText = `Total:${quantity * 799}`
+}
+
+
+// product2
+const quantityText2 = document.getElementById("quantity-text2");
+const showQuantity2 = document.getElementById("show-quantity2");
+const showTotal2 = document.getElementById("show-total2");
+
+function inc2()
+{
+
+let quantity = parseInt(quantityText2.innerText);
+
+if(quantity < 5)
+{
+  quantityText2.innerText = ++quantity;
+}
+else
+{
+  alert("you cant add more than 5 items")
+}
+showQuantity2.innerText = `${quantity} x 799`;
+showTotal2.innerText = `Total:${quantity * 799}`
+}
+
+function dec2()
+{
+let quantity = parseInt(quantityText2.innerText);
+
+if(quantity > 1)
+{
+  quantityText2.innerText = --quantity;
+}
+else
+{
+  alert("you cant add less than 1 items")
+}
+showQuantity2.innerText = `${quantity} x 799`
+showTotal2.innerText = `Total:${quantity * 799}`
 }
